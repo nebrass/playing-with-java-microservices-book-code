158c0187208c3bfcb1ba980a043e4c9d90487209
Nebrass <lnibrass@gmail.com>
2018-11-26 19:29:38 +0100

    Minor updates

--- Api-gateway/src/main/java/com/targa/labs/dev/apigateway/ApiGatewayApplication.java
+++ Api-gateway/src/main/java/com/targa/labs/dev/apigateway/ApiGatewayApplication.java
@@ -1,16 +1,40 @@
 package com.targa.labs.dev.apigateway;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
-@EnableSwagger2
-@EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ApiGatewayApplication {
 
+    private static final Log log = LogFactory.getLog(ApiGatewayApplication.class);
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @Bean
+    public DiscoveryClientRouteDefinitionLocator discoveryClientRouteLocator(DiscoveryClient discoveryClient,
+            DiscoveryLocatorProperties properties) {
+        return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
